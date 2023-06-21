1a6eb8c3cbf5c04f57af113dfdb7bfb15d0ee33f
Alex Sheiko <kelamir@protonmail.com>
2023-06-21 16:30:17 +0300

    fix: styling not being applied

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 // pages/_app.js
 import type { AppType } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
+import './globals.css';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
