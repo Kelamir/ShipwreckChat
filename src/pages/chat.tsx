9ef1c06a2f6c5f8603fabcefcb21eeb0607a058d
Alex Sheiko <kelamir@protonmail.com>
2023-06-22 16:58:10 +0300

    feat: store user in db on login

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,8 +1,36 @@
 import ChatList from '@/components/ChatList';
-import MessageList from '@/components/MessageList';
-import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
+import { trpc } from '@/utils/trpc';
+import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0/client';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 function Chat() {
+  const { user, isLoading } = useUser();
+  const storeUserMutation = trpc.storeUser.useMutation({
+    onSuccess: () => {
+      console.log('done');
+    },
+  });
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoading && user) {
+      storeUserMutation
+        .mutateAsync({
+          auth0Id: '1',
+          email: user.email as string,
+          username: user.nickname || user.name || (user.email as string), // from auth0
+        })
+        .then(() => {
+          alert('stored user in pg db');
+        })
+        .catch((err: any) => {
+          // Handle errors during user storage
+          console.error('Error storing user:', err);
+        });
+    }
+  }, [user, isLoading, storeUserMutation, router]);
+
   return (
     <div>
       <ChatList />
