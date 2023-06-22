7eafa3d1910ba852352972e4f55651a376928ef1
Alex Sheiko <kelamir@protonmail.com>
2023-06-22 15:18:57 +0300

    feat: API routes to add users to pg db on login

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -5,8 +5,50 @@
 import * as trpcNext from '@trpc/server/adapters/next';
 import { publicProcedure, router } from '@/server/trpc';
 import { z } from 'zod';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { PrismaClient } from '@prisma/client';
+import { TRPCError } from '@trpc/server';
 
-const appRouter = router({});
+const prisma = new PrismaClient();
+
+const appRouter = router({
+  getUser: publicProcedure.query(() => {
+    const { isLoading, user } = useUser();
+
+    if (!isLoading) {
+      return user;
+    }
+  }),
+
+  storeUser: publicProcedure
+    .input(
+      z.object({
+        auth0Id: z.string(),
+        email: z.string(),
+        username: z.string(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const existingUser = await prisma.user.findUnique({
+        where: { email: input.email },
+      });
+
+      if (existingUser) {
+        throw new TRPCError({
+          message: 'User already exists',
+          code: 'PRECONDITION_FAILED',
+        });
+      }
+
+      const user = await prisma.user.create({
+        data: {
+          auth0Id: input.auth0Id,
+          email: input.email,
+          username: input.username,
+        },
+      });
+    }),
+});
 
 // export only the type definition of the API
 // None of the actual implementation is exposed to the client
